00ac236a12a2094b0af27621b7f8fa2f86797c69
OsamaHIma <osamahima018@gmail.com>
2025-05-07 09:21:51 +0300

    push

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,6 +14,7 @@ export async function generateMetadata({
   const locale = (await params).locale;
   return {
     metadataBase: new URL("https://osama-ibrahim-portfolio.vercel.app"),
+    manifest: "/manifest.json",
     title:
       locale === "ar"
         ? "أسامة إبراهيم | مطور واجهات أمامية"
@@ -34,27 +35,25 @@ export async function generateMetadata({
     keywords:
       locale === "ar"
         ? "مطور واجهات أمامية، مطور ويب، مطور رياكت، مطور نكست جي اس، مطور واجهة مستخدم، خبير جافاسكريبت، تايبسكريبت، تطوير الويب، رياكت، تطبيقات ويب حديثة، تصميم متجاوب، تطوير واجهة المستخدم، هندسة الواجهات الأمامية، تطوير المواقع، برمجة الويب، مطور مصري، تطوير مواقع عربية، مطور ثنائي اللغة"
-        : "Front-end Developer, Web Developer, React Developer, Next.js Developer, UI Developer, JavaScript Expert, TypeScript, Web Development, React.js, Modern Web Applications, Responsive Design, User Interface Development, Frontend Engineering, Website Development, Web Programming, Egypt Developer, Arabic Website Development, Bilingual Developer",
+        : "Front-end Developer, Osama ibrahim portfolio, Web Developer, React Developer, Next.js Developer, UI Developer, JavaScript Expert, TypeScript, Web Development, React.js, Modern Web Applications, Responsive Design, User Interface Development, Frontend Engineering, Website Development, Web Programming, Egypt Developer, Arabic Website Development, Bilingual Developer",
     creator: locale === "ar" ? "أسامة إبراهيم" : "Osama Ibrahim",
   };
 }
 
 export default function Home() {
   return (
-
-      <div className="relative flex justify-center items-center flex-col overflow-clip mx-auto max-lg:pb-13">
-        <Navbar />
-        <MobileNav />
-        <InstallPrompt />
-        <main className="max-w-7xl w-full sm:px-10 px-5 ">
-          <Hero />
-          <Projects />
-          <div className="absolute -z-[1] top-[50%] left-[5%] size-80 bg-blue-500/30 blur-[100px]" />
-          <div className="absolute -z-[1] bottom-[14%] sm:bottom-[10%] right-[5%] size-80 bg-gradient-to-tr from-red-200 from-40% via-60% via-blue-600 to-purple-500 blur-[93px]" />
-          <Experience />
-        </main>
-        <Footer />
-      </div>
-
+    <div className="relative flex justify-center items-center flex-col overflow-clip mx-auto max-lg:pb-13">
+      <Navbar />
+      <MobileNav />
+      <InstallPrompt />
+      <main className="max-w-7xl w-full sm:px-10 px-5 ">
+        <Hero />
+        <Projects />
+        <div className="absolute -z-[1] top-[50%] left-[5%] size-80 bg-blue-500/30 blur-[100px]" />
+        <div className="absolute -z-[1] bottom-[14%] sm:bottom-[10%] right-[5%] size-80 bg-gradient-to-tr from-red-200 from-40% via-60% via-blue-600 to-purple-500 blur-[93px]" />
+        <Experience />
+      </main>
+      <Footer />
+    </div>
   );
 }
